3e029b027b08a52eec66605633541d8a47128aea
christian0418 <tanchristian_bsit@plmun.edu.ph>
2025-02-19 13:44:03 +0800

    for_loop_break_continue.py

--- for_loop_break_continue.py
+++ for_loop_break_continue.py
@@ -1,8 +1,6 @@
 for num in numbers:
     if num == 3:
         continue  # Skip the rest of this iteration when num is 3
-
     if num == 7:
         break  # Exit the loop completely when num is 7
-
-    print(num)  # Print the number if it's not skipped or breaking the loop
+    print(num)  
